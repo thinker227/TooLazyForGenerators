46ebb73f9c257f1ae26756fd57d314d5d03ccce2
thinker227 <thinker0227@gmail.com>
2023-04-15 23:28:23 +0200

    Add FilesAsDictionary

--- src/TooLazyForGenerators/GeneratorOutputExtensions.cs
+++ src/TooLazyForGenerators/GeneratorOutputExtensions.cs
@@ -1,10 +1,36 @@
-﻿namespace TooLazyForGenerators;
+﻿using Microsoft.CodeAnalysis;
+
+namespace TooLazyForGenerators;
 
 /// <summary>
 /// Extensions for <see cref="GeneratorOutput"/>.
 /// </summary>
 public static class GeneratorOutputExtensions
 {
+    /// <summary>
+    /// Returns the files of a <see cref="GeneratorOutput"/> as a dictionary of projects and files.
+    /// </summary>
+    /// <param name="output">The source output.</param>
+    public static IReadOnlyDictionary<Project, IReadOnlyCollection<SourceFile>> FilesAsDictionary(
+        this GeneratorOutput output)
+    {
+        var result = new Dictionary<Project, IReadOnlyCollection<SourceFile>>();
+
+        foreach (var file in output.Files)
+        {
+            if (!result.TryGetValue(file.Project, out var files))
+            {
+                files = new List<SourceFile>();
+                result.Add(file.Project, files);
+            }
+
+            var list = (List<SourceFile>)files;
+            list.Add(file.File);
+        }
+
+        return result;
+    }
+    
     /// <summary>
     /// Writes the output of a <see cref="GeneratorOutput"/> to disk and returns
     /// a status code depending on whether the output was successful or not.
