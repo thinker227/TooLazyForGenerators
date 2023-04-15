7204a581c72d8f5e24f114f506e5d7f9ceede08c
thinker227 <thinker0227@gmail.com>
2023-04-15 15:57:30 +0200

    Implement LazyGeneratorBuilder

--- src/TooLazyForGenerators/LazyGenerator.cs
+++ src/TooLazyForGenerators/LazyGenerator.cs
@@ -2,5 +2,9 @@
 
 internal sealed class LazyGenerator : ILazyGenerator
 {
+    public required IReadOnlyCollection<FileInfo> ProjectFiles { get; init; }
+    
+    public required IReadOnlyCollection<Type> Outputs { get; init; }
+
     public Task<GeneratorOutput> Run(CancellationToken cancellationToken = default) => throw new NotImplementedException();
 }
--- src/TooLazyForGenerators/LazyGeneratorBuilder.cs
+++ src/TooLazyForGenerators/LazyGeneratorBuilder.cs
@@ -5,21 +5,36 @@
 /// </summary>
 public sealed class LazyGeneratorBuilder
 {
+    private readonly List<FileInfo> projectFiles = new();
+    private readonly List<Type> outputs = new();
+
     /// <summary>
     /// Adds a project for the generator to target.
     /// </summary>
     /// <param name="projectFile">The project (<c>.csproj</c>) file of the project.</param>
-    public LazyGeneratorBuilder TargetingProject(FileInfo projectFile) => throw new NotImplementedException();
+    public LazyGeneratorBuilder TargetingProject(FileInfo projectFile)
+    {
+        projectFiles.Add(projectFile);
+        return this;
+    }
 
     /// <summary>
     /// Registers an output for the generator to use.
     /// </summary>
     /// <param name="outputType">The type of the output to register.
     /// The type has to implement <see cref="ISourceOutput{TSelf}"/>.</param>
-    public LazyGeneratorBuilder WithOutput(Type outputType) => throw new NotImplementedException();
+    public LazyGeneratorBuilder WithOutput(Type outputType)
+    {
+        outputs.Add(outputType);
+        return this;
+    }
 
     /// <summary>
     /// Builds the generator.
     /// </summary>
-    public ILazyGenerator Build() => throw new NotImplementedException();
+    public ILazyGenerator Build() => new LazyGenerator()
+    {
+        ProjectFiles = projectFiles,
+        Outputs = outputs
+    };
 }
