cfc9f83f93d65954e1bae175a829f11065d895d9
thinker227 <thinker0227@gmail.com>
2023-05-01 01:06:54 +0200

    Add comments to Program.cs

--- sample/TooLazyForGenerators.Sample.Generator/Program.cs
+++ sample/TooLazyForGenerators.Sample.Generator/Program.cs
@@ -1,8 +1,12 @@
 ﻿using TooLazyForGenerators;
 
 var builder = new LazyGeneratorBuilder();
+// Target the project TooLazyForGenerators.Sample.
 await builder.TargetingProjectWithName("TooLazyForGenerators.Sample");
+// Use all outputs from the current assembly.
 builder.WithOutputsFromAssembly();
 
+// Build the generator and run it.
 using var output = await builder.Build().Run();
+// Write the output to disk and return a status code.
 return output.WriteAndReturn();
