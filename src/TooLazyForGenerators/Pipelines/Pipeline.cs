1e91214e5525e29c4871f9807c30fe118c1c2ab5
thinker227 <thinker0227@gmail.com>
2023-04-30 15:00:15 +0200

    Add documentation to pipeline delegates

--- src/TooLazyForGenerators/Pipelines/Pipeline.cs
+++ src/TooLazyForGenerators/Pipelines/Pipeline.cs
@@ -1,5 +1,15 @@
 ﻿namespace TooLazyForGenerators.Pipelines;
 
+/// <summary>
+/// A delegate for a pipeline step.
+/// </summary>
+/// <param name="ctx">The output context passed through the pipeline.</param>
+/// <param name="next">The next step in the pipeline.
+/// Call this function to continue the pipeline, or do not to abort it.</param>
 public delegate Task PipelineStep(ISourceOutputContext ctx, PipelineContinuation next);
 
+/// <summary>
+/// A delegate which continues a pipeline.
+/// </summary>
+/// <param name="ctx">The output context to pass to the next pipeline step.</param>
 public delegate Task PipelineContinuation(ISourceOutputContext ctx);
