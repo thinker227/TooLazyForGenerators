ea3bdd4a5e911bdf094a3f4e8e60c0e95ae03430
thinker227 <thinker0227@gmail.com>
2023-04-30 15:04:11 +0200

    Add PipelineExtensions

---
+++ src/TooLazyForGenerators/Pipelines/PipelineExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace TooLazyForGenerators.Pipelines;
+
+/// <summary>
+/// Extensions relating to pipelines. 
+/// </summary>
+public static class PipelineExtensions
+{
+    /// <summary>
+    /// Adds a pipeline step to the builder which filters for projects with a specific language.
+    /// </summary>
+    /// <param name="builder">The source builder.</param>
+    /// <param name="languageName">The name of the language to filter for.
+    /// It is recommended to use <see cref="Microsoft.CodeAnalysis.LanguageNames"/> for common language names.</param>
+    public static LazyGeneratorBuilder ForLanguage(this LazyGeneratorBuilder builder, string languageName) =>
+        builder.Using((ctx, next) =>
+            ctx.Project.Language == languageName
+                ? next(ctx)
+                : Task.CompletedTask);
+}
