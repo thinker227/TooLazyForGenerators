fa0f92a44cf02f671ea6f9ef5bf1aea83b8635b2
thinker227 <thinker0227@gmail.com>
2023-05-02 23:21:06 +0200

    Add UsingDependencyInjection extension

--- src/TooLazyForGenerators/Pipelines/PipelineExtensions.cs
+++ src/TooLazyForGenerators/Pipelines/PipelineExtensions.cs
@@ -16,4 +16,28 @@ public static class PipelineExtensions
             ctx.Project.Language == languageName
                 ? next(ctx)
                 : Task.CompletedTask);
+        
+        return builder;
+    }
+
+    /// <summary>
+    /// Adds dependency injection to the generator pipeline.
+    /// </summary>
+    /// <param name="builder">The source builder.</param>
+    public static TBuilder UsingDependencyInjection<TBuilder>(
+        this TBuilder builder)
+        where TBuilder : IPipelineBuilder
+    {
+        builder.Using((ctx, next) =>
+        {
+            ctx.CreateTarget = creationCtx =>
+                (ISourceOutput)ActivatorUtilities.CreateInstance(
+                    creationCtx.Services,
+                    creationCtx.TargetType);
+            
+            return next(ctx);
+        });
+        
+        return builder;
+    }
 }
